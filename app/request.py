83173aee554dae72b8b5b97dfa05e761170e8fa6
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-04-26 12:17:56 +0300

    ft: add a function to process our results list to type Movie object list

--- app/request.py
+++ app/request.py
@@ -1,3 +1,4 @@
+from turtle import title
 from app import app
 import urllib.request,json
 from .models import movie
@@ -25,5 +26,33 @@ def get_movies(category):
         if get_movies_response['results']:
             movie_results_list = get_movies_response['results']
             movie_results = process_results(movie_results_list)
-            
+
+    return movie_results
+
+def process_results(movie_results_list):
+    '''
+    Function that processes the movie results and transform them to a list of objects
+
+    Args:
+         movie_results_list: A list of dictionaries that contain movie details
+    
+    Returns:
+         movie_results: A list of movie objects
+
+    '''
+
+    movie_results = []
+
+    for movie_item in movie_results_list:
+        id = movie_item.get('id')
+        title = movie_item.get('original_title')
+        overview = movie_item.get('overview')
+        poster = movie_item.get('poster_path')
+        vote_average = movie_item.get('vote_average')
+        vote_count = movie_item.get('vote_count')
+        
+        if poster:
+            movie_object = Movie(id, title, overview, poster, vote_average, vote_count)
+            movie_results.append(movie_object)
+
     return movie_results
