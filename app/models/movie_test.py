294b0415aad62b2aa52b1e1c092cdb1247d33eb3
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-04-26 11:26:34 +0300

    ft: add test for a new instance movie

---
+++ app/models/movie_test.py
@@ -0,0 +1,25 @@
+import unittest
+from models import movie
+Movie = movie.Movie
+
+class MovieTest(unittest.TestCase):
+    '''
+    Test class to test behaviour of the movie class
+    '''
+    def setUp(self):
+        '''
+        Set up method that will run before every test
+        '''
+        self.new_movie = Movie(
+        1234, 
+        'Python Must Be Crazy', 'A thrillling new python series', 
+        'https://image.tmdb.org/t/p/w500/khsjha27hbs', 
+        8.5, 
+        129993
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie, Movie))
+
+if __name__ == '__main__':
+    unittest.main()
