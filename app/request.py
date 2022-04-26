9051429a1fd75d3b16dbd1f2721e83403a591ce4
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-04-26 11:43:29 +0300

    ft: add necessary imports to request.py file

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import movie
 
 #getting api key
 api_key = app.config['MOVIE_API_KEY']
+#getting the base_url
+base_url = app.config['MOVIE_API_BASE_URL']
+
+#Get Movie class template
+Movie = movie.Movie
