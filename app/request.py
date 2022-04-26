a1252283eb61a0c34e04855fe40c67cc20c2988c
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-04-26 11:48:26 +0300

    ft: join the movies base url with api key and category

--- app/request.py
+++ app/request.py
@@ -9,3 +9,9 @@ base_url = app.config['MOVIE_API_BASE_URL']
 
 #Get Movie class template
 Movie = movie.Movie
+
+def get_movies(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_movies_url = base_url.format(category, api_key)
